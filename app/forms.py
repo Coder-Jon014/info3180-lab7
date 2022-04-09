8aa7a2a7c9d27943c3d87948bd36902970354b84
Jon-Daniel Coombs <71227832+Coder-Jon014@users.noreply.github.com>
2022-04-09 02:23:26 -0500

    Added Validator Check for Description field

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,11 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import TextAreaField
+from wtforms.validators import DataRequired
 
 
 class UploadForm(FlaskForm):
-    description = TextAreaField('Description')
+    description = TextAreaField('Description', validators=[DataRequired() ])
     photo = FileField('Photo', validators=[
         FileRequired(),
         FileAllowed(['jpg','jpeg', 'png'], 'Images only!')
